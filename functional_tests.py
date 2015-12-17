a257aa13b963b9801b8d2a06d7c60aad6849a9c0
Sean Campbell <sean.campbell1@maine.edu>
2015-12-17 15:01:33 -0500

    To-Do lists now saves post

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		self.browser.get('http://localhost:8000')
@@ -27,15 +33,16 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		#Test to see if we can enter a new item
+		inputbox.send_keys('Buy a peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		check_for_row_in_list_table('1: Buy a peacock feathers')
+
+		#Test to see if we can enter a second item
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy a peacock feathers', [row.text for row in rows])
-		self.assertIn(
-        	'2: Use peacock feathers to make a fly' ,
-         	[row.text for row in rows]
-    	)	
+		check_for_row_in_list_table('1: Buy a peacock feathers')
+		check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		
 
 		# There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
