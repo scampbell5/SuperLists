b33da06dfea03670aafc502b8e709d52ae85e833
Sean Campbell <sean.campbell1@maine.edu>
2015-12-17 14:54:20 -0500

    To-Do lists now saves post

--- functional_tests.py
+++ functional_tests.py
@@ -27,19 +27,19 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		#Test to see if we can enter a new item
-		inputbox.send_keys('Buy a peacock feathers')
+		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy a peacock feathers' for row in rows)
-		)
+		self.assertIn('1: Buy a peacock feathers', [row.text for row in rows])
+		self.assertIn(
+        	'2: Use peacock feathers to make a fly' ,
+         	[row.text for row in rows]
+    	)	
 
 		# There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-		tearDown()
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
