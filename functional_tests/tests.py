fa80e4c58ad0ce311c18883aa16f41648be2b558
Sean Campbell <sean.campbell1@maine.edu>
2015-12-29 13:12:27 -0500

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,19 +4,18 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(LiveServerTestCase):
 
-	browser = webdriver.Chrome()
 
 	def setUp(self):
+		self.browser = webdriver.Chrome()
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
 
-
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+		self.assertIn(row_text, [row.text for row in rows])	
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -70,6 +69,20 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn("Buy peacock feathers", page_text)
 		self.assertIn("Buy milk", page_text)
 
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)	
+
+		inputbox.send_keys("testing\n")
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)	
+
+		
+			
 
 
 
