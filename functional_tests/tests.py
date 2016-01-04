cb711a4a2dfc0bda2c5c7ee3071538ba7b04bbe6
Sean Campbell <sean.campbell1@maine.edu>
2016-01-04 16:15:20 -0500

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,24 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 
 	def setUp(self):
@@ -19,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		#The page title and header both mentions 'To-Do lists'
 		self.assertIn('To-Do', self.browser.title)
@@ -51,7 +67,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		## New user comes along and opens a new browser
 		## Tests to make sure previous user's list is not there
 		self.browser = webdriver.Chrome()
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn("Buy peacock feathers", page_text)
 		self.assertNotIn("make a fly", page_text)
@@ -70,7 +86,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn("Buy milk", page_text)
 
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
