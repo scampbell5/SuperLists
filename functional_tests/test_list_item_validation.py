28d4a5ab797772d3fcb24e7e3c322fc6488ee187
Sean Campbell <sean.campbell1@maine.edu>
2016-01-06 13:35:49 -0500

    Moved Functional Tests into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,14 @@
+from .base import FunctionalTest
+from unittest import skip
+
+class ItemValidationTest(FunctionalTest):
+	
+	def test_cannot_add_empty_list_item(self):
+		self.fail('write me!')
+
+		
+			
+
+
+
+
